4b8590dc5a3b3416222a677d2538289f3b55a2d0
dbaranov7658 <dima.baranov.2016@inbox.ru>
2022-02-16 19:23:26 -0800

    adding functionality 12

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
-var pool;
 
-pool = new Pool({
-    connectionString: process.env.DATABASE_URl
-})
+const pool = new Pool({
+    user: process.env.PG_USER,      //postgres user
+    host: process.env.PG_ENDPOINT,  //localhost (I also tried 127.0.0.1)
+    database: process.env.PG_DB,    //database name to connect to
+    password: process.env.PG_PASS,  //postgres user password
+    port: process.env.PG_PORT       //5432
+});
 var uuid = require('uuid');
 
 var app = express()
